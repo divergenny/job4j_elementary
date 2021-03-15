f68d579b53ebb84d30e9b366676f0a22a34fb210
divergenny <divergenny@gmail.com>
2021-03-15 14:45:41 +0500

    1. Модульные тесты. [#497721]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -7,32 +7,32 @@ public class Converter {
 
     public static double rubleToEuro(int value) {
         double result = (double) value / EURO; /* формула перевода рублей в евро. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static double rubleToDollar(int value) {
         double result = (double) value / DOLLAR; /* формула перевода рублей в доллары. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static double rubleToUzs(int value) {
         double result = (double) value * UZS; /* формула перевода рублей в узбекские суммы. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static double euroToRuble(int value) {
         double result = (double) value * EURO; /* формула перевода евро в рубли. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static double dollarToRuble(int value) {
         double result = (double) value * DOLLAR; /* формула перевода долларов в рубли. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static double uzsToRuble(int value) {
         double result = (double) value / UZS; /* формула перевода узбекских сум в рубли. */
-        return Math.round(result);
+        return (double) Math.round(result * 100) / 100;
     }
 
     public static void main(String[] args) {
@@ -42,19 +42,19 @@ public class Converter {
         double expected = 2.0;
         double out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
-        System.out.println("140 rubles are 2.000 euro. Test result : " + passed);
+        System.out.println("140 rubles are 2.00 euro. Test result : " + passed);
 
         in = 140;
-        expected = 2.0;
+        expected = 2.33;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
-        System.out.println("140 rubles are 2.333 dollars. Test result : " + passed);
+        System.out.println("140 rubles are 2.33 dollars. Test result : " + passed);
 
         in = 140;
-        expected = 20014.0;
+        expected = 20014.4;
         out = Converter.rubleToUzs(in);
         passed = expected == out;
-        System.out.println("140 rubles are 20014.0 uzbek sums. Test result : " + passed);
+        System.out.println("140 rubles are 20014.4 uzbek sums. Test result : " + passed);
 
         in = 140;
         expected = 9800.0;
@@ -69,10 +69,10 @@ public class Converter {
         System.out.println("140 dollar are 8400.0 ruble. Test result : " + passed);
 
         in = 140;
-        expected = 1.0;
+        expected = 0.98;
         out = Converter.uzsToRuble(in);
         passed = expected == out;
-        System.out.println("140 uzbek sums are 1.0 ruble. Test result : " + passed);
+        System.out.println("140 uzbek sums are 0.98 ruble. Test result : " + passed);
 
         System.out.println("\n===== TEST Code End =====\n");
 
---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        double expected = 2.0;
+        double out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert180RblThen2Point57Euro() {
+        int in = 180;
+        double expected = 2.57;
+        double out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert140RblThen2Point33Dollar() {
+        int in = 140;
+        double expected = 2.33;
+        double out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert140RblThen20014Point4Uzs() {
+        int in = 140;
+        double expected = 20014.4;
+        double out = Converter.rubleToUzs(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert140EuroThen9800Ruble() {
+        int in = 140;
+        double expected = 9800;
+        double out = Converter.euroToRuble(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert140DollarThen8400Ruble() {
+        int in = 140;
+        double expected = 8400;
+        double out = Converter.dollarToRuble(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+
+    @Test
+    public void whenConvert140UzsThen0Point98Ruble() {
+        int in = 140;
+        double expected = 0.98;
+        double out = Converter.uzsToRuble(in);
+        Assert.assertEquals(expected, out, 0);
+    }
+}

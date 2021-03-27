083467e1cefde6df0e15336470d75569281dee50
divergenny <divergenny@gmail.com>
2021-03-27 13:48:29 +0500

    4.2. Тернарное сравнение [#497648] Fixed return statement ternary operation.

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,11 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        if (left == right) {
-            return left;
-        } else {
-            return left > right ?  left :  right;
-        }
+        return left >= right ? left : right;
     }
 
     public static void main(String[] args) {

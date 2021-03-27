b72f66385ce051162e3330f7add04b68b4fe9333
divergenny <divergenny@gmail.com>
2021-03-27 22:12:11 +0500

    4.3.6. Math.abs [#497647]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) { //elephant way
+        int rsl = 0;
+        if (x1 != x2 || y1 != y2) {
+            rsl = Math.abs(x2 - x1) == Math.abs(y2 - y1) ? Math.abs(x2 - x1) : 0;
+        }
+        return rsl;
+    }
+}

a1baf10920e5028054fd7bbb1bdb7f2ddf498495
divergenny <divergenny@gmail.com>
2021-03-29 13:30:09 +0500

    5.5. Простое число [#497748]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -5,7 +5,7 @@ public class CheckPrimeNumber {
         number = Math.abs(number);
         boolean prime = true;
         if (number == 1) {
-            prime = false;
+            return false;
         }
         for (int index = 2; index < number; index++) {
             if (number % index == 0) {

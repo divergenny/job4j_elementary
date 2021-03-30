c75f200827ebaaa3e2cbec8f33c6dbce346dcbb2
divergenny <divergenny@gmail.com>
2021-03-30 11:40:30 +0500

    5.9.1. Генерация конструкций в IDEA [#497752]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) { // psvm
+        for (int i = 0; i <= 10; i++) { // fori
+            System.out.println(i); //sout
+        }
+    }
+}

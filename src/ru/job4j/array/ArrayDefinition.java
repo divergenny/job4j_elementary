a74fc3f20547779600bda6ea19405dda966f8b8f
divergenny <divergenny@gmail.com>
2021-03-30 14:12:44 +0500

    6.0.2. Заполнение массива. [#497669]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Jamshid";
+        names[1] = "Bakhrom";
+        names[2] = "Kudrat";
+        names[3] = "Petr";
+
+        for (int var = 0; var < 4; var++) {
+            System.out.println(names[var]);
+        }
 
         System.out.println("Size of array ages: " + ages.length);
         System.out.println("Size of array surnames: " + surnames.length);
